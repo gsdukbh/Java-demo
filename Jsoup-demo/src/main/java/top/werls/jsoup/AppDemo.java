1b69574cbb983ad46fe095730c98c41d89727828
li we <gsdukbh@gmail.com>
2022-06-03 13:41:28 +0000

    add

--- Jsoup-demo/src/main/java/top/werls/jsoup/AppDemo.java
+++ Jsoup-demo/src/main/java/top/werls/jsoup/AppDemo.java
@@ -13,15 +13,14 @@ public class AppDemo {
 
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.bing.com/search")
-                .userAgent("Mozilla")
+                .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36")
                 .data("q", "圣墟")
                 .get();
 //        System.out.println(doc.body().getElementById("b_content"));
         Element content = doc.body().getElementById("b_content");
-        
 
-//        System.out.println(doc.getElementsByClass("b_algo"));
-
+//        System.out.println(content.html());
+        System.out.println(doc.getElementsByClass("b_algo"));
 
     }
 }
